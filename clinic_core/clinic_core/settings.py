e4f844496b57259dfe98331d4036bc2766fbfb9e
elsawyFullStack <elsawylancer951@gmail.com>
2022-03-13 17:31:25 +0200

    setup database

--- clinic_core/clinic_core/settings.py
+++ clinic_core/clinic_core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'clinic_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'clinic_system_db',
+        'USER': 'manager',
+        'PASSWORD': 'manager',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
